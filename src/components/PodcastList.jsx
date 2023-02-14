9e058ce07dbea5b47dfdb58359f4a4262f471e26
Ramón Espinosa <respinosa@hiberus.onmicrosoft.com>
2023-02-14 11:07:49 +0100

    Fix: Eliminamos código duplicado y sin usar

--- src/components/PodcastList.jsx
+++ src/components/PodcastList.jsx
@@ -6,7 +6,6 @@ import { SearchBar } from "./SearchBar";
 import * as API from "../services/podcasts";
 
 export function PodcastList() {
-  const [podcastItem, setPodcastItem] = useState([]);
   const [podcasts, setPodcasts] = useState([]);
   const [filteredPodcasts, setFilteredPodcasts] = useState([]);
   const [filterText, setFilterText] = useState("");
@@ -39,13 +38,6 @@ export function PodcastList() {
     );
   }, [filterText, podcasts]);
 
-  const handlePodcastClick = (podcastId) => {
-    // Aquí se puede agregar la lógica necesaria para navegar a la vista con el detalle del podcast.
-    API.getPodcastById(podcastId).then((data) => {
-      setPodcastItem(data);
-    });
-  };
-
   return (
     <>
       <SearchBar
@@ -54,28 +46,21 @@ export function PodcastList() {
         filteredPodcasts={filteredPodcasts}
       />
       <div className="podcasts-list">
-        {podcasts
-          .filter((podcast) =>
-            podcast["im:name"].label
-              .toLowerCase()
-              .includes(filterText.toLowerCase())
-          )
-          .map((podcast) => (
-            <div
-              className="podcasts-list__item"
-              key={podcast.id.attributes["im:id"]}
-              //onClick={() => handlePodcastClick(podcast.id.attributes["im:id"])}
-            >
-              <Link to={`/podcast/${podcast.id.attributes['im:id']}`}>
-                <img
-                  src={podcast["im:image"][2].label}
-                  alt={podcast.title.label}
-                />
-                <h3>{podcast["im:name"].label}</h3>
-                <p>Author: {podcast["im:artist"].label}</p>
-              </Link>
-            </div>
-          ))}
+        {filteredPodcasts.map((podcast) => (
+          <div
+            className="podcasts-list__item"
+            key={podcast.id.attributes["im:id"]}
+          >
+            <Link to={`/podcast/${podcast.id.attributes["im:id"]}`}>
+              <img
+                src={podcast["im:image"][2].label}
+                alt={podcast.title.label}
+              />
+              <h3>{podcast["im:name"].label}</h3>
+              <p>Author: {podcast["im:artist"].label}</p>
+            </Link>
+          </div>
+        ))}
       </div>
     </>
   );
